f440b0e1b7bbd527a8025de19244340e8b72db74
Johan Zeeck <jzeeck@kth.se>
2012-03-01 11:22:46 +0100

    Made changes so the config reads the TaggingController class and moved the code from UserController to TaggingController

--- controllers/tagging_controller.rb
+++ controllers/tagging_controller.rb
@@ -3,19 +3,33 @@ require_relative 'application_controller'
 class TaggingController < ApplicationController
 	
 
-	#in general define exactly what these does
-	#how should they work, should they be changed
-	#should it be a web request to the server with the edit request?
-	#should it be run in a hidden frame on the /tagging side and be changed with JQuary?
-	get '/tagging?limit=:number' do
-
+	
+	#This route should pull json object from the corpus and dispaly them in the appropriate
+	#layout.
+	#All posts should be stored in a session variable namely session[:msgs]
+	get %r{/tagging/limit/(\d{1,99999})} do |limit|
+  		mustache :tagging
+	end
+	
+	#This should dispay the messages that session[:msgs] contains on the same page
+	#as the %r{/tagging/limit/(\d{1,99999})} displays
+	get '/tagging' do 
+		"Working!"
+	end
+	
+	#This route will commit a json object to the training set so that the algorythms can work with it.
+	#The id should be a ID of an object in the session[:msgs]
+	#If the ID was not found in this variable the user should be notified.
+	#
+	#After this commit it done the session[:msgs] should be updated and the tagging page should be loaded
+	post %r{/tagging/commit/(\d{1,99999})} do |id|
+		"Working, commit id was #{id}"
 	end
 
-	get '/tagging/edit?id=:id' do
-
-	end
-	#update what?
-	post '/tagging/update' do
-
+	#Removes a message form the database and from the session[:msgs]
+	#
+	#After this discard it done the session[:msgs] should be updated and the tagging page should be loaded
+	post %r{/tagging/discard/(\d{1,99999})} do |id|
+		"Working, discard id was #{id}"
 	end
 end
