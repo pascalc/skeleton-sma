f70edf29d23df06543646eaa9724e04638600fb6
Johan Zeeck <jzeeck@kth.se>
2012-03-02 12:12:26 +0100

    changed some data for the mustache side added a dummy object so the team can work with it.

--- controllers/tagging_controller.rb
+++ controllers/tagging_controller.rb
@@ -2,8 +2,6 @@ require_relative 'application_controller'
 
 class TaggingController < ApplicationController
 	
-
-	
 	#This route should pull json object from the corpus and dispaly them in the appropriate
 	#layout.
 	#All posts should be stored in a session variable namely session[:msgs]
@@ -14,7 +12,7 @@ class TaggingController < ApplicationController
 	#This should dispay the messages that session[:msgs] contains on the same page
 	#as the %r{/tagging/limit/(\d{1,99999})} displays
 	get '/tagging' do 
-
+		session[:msgs] = [{"source" => "twitter", "last_modified"=> "2012-03-02T10:17:46.182000", "_new"=> false, "keywords"=> ["lol", "there", "all", "dead", "#thefrenchmistake", "#spn"], "text"=> "LOl there all dead #TheFrenchMistake #SPN", "created_at"=> "Fri Mar 02 10:16:56 +0000 2012", "_id"=> 568475}]
 		#if the session[:msgs] is empty we should get some
 		mustache :tagging
 	end
