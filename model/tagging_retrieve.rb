bc9838e3d8168fd3d347c5a1970fe1e5301de0af
Jonatan Svensson <jonatansve@gmail.com>
2012-03-02 13:20:18 +0100

    RetrieveClassificationObjects

---
+++ model/tagging_retrieve.rb
@@ -0,0 +1,1 @@
+class tagging_retrieve			include HTTParty	base_uri "http://nosy.pspace.se/corpus"		def RetrieveClassificationObjects(limit1)		return self.class.get(:query => {:limit => limit1)	end	end
