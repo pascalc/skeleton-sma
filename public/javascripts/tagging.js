8ab200ce6018cf9c8b2ec99725c53b6a1de3f131
Johan Zeeck <jzeeck@kth.se>
2012-03-07 10:57:36 +0100

    added hover css property and click on list element change color.

--- public/javascripts/tagging.js
+++ public/javascripts/tagging.js
@@ -3,6 +3,11 @@ var lastPostID;
 function showInfo(postID) {
     var element=  $('#'+'details' + postID);
     element.removeClass('hidden');
+    element.hide();
+    element.fadeIn('fast');
+
+
+    $('#'+'shortInfo' + postID).addClass('selected');
 
     var tempID1 = parseInt(lastPostID);
     var tempID2 = parseInt(postID);
@@ -14,5 +19,6 @@ function showInfo(postID) {
 function hideInfo(postID) {
     var element=  $('#'+'details' + postID);
     element.addClass('hidden');
+    $('#'+'shortInfo' + postID).removeClass('selected');
 }
 
