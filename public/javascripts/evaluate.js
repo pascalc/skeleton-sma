2b3efb03e2fdfa00583d9e7d45cdcd83f3bef66f
anders elovsson <anderselowsson@gmail.com>
2012-03-09 10:24:40 +0100

    Small Changes, updated functionality for javascript

--- public/javascripts/evaluate.js
+++ public/javascripts/evaluate.js
@@ -1,18 +1,23 @@
-var prevID;
+var lastPostID;
 
-function showFullMessage(ID)
-{
-	var element = $('#' + 'full' + ID);
-	element.removeClass('hidden');
-	element.hide();
-	element.fadeIn('fast');
+function showFullMessage(postID){
+    var element = $('#' + 'full' + postID);
+    element.removeClass('hidden');
+    element.hide();
+    element.fadeIn('fast');
 
-	$('#'+'partial' + ID).addClass('selected');
 
-	var prev = parseInt(prevID);
-	var current = parseInt(ID);
-	if(current!=prev){
-		hideInfo(prevID)
-	}
-	prevID = ID;
+    $('#'+'partial' + postID).addClass('selected');
+
+    var tempID1 = parseInt(lastPostID);
+    var tempID2 = parseInt(postID);
+    if(tempID1!=tempID2){			//Kollar att de två senaste posterna ej samma
+        hideInfo(lastPostID)			//Om ej samma, hideInfo - förra			
+    }
+    lastPostID = postID;
+}
+function hideInfo(postID) {
+    var element=  $('#'+'full' + postID);		//gör till element..
+    element.addClass('hidden'); 			//Ska vara hidden
+    $('#'+'partial' + postID).removeClass('selected');	//info ej selected
 }
