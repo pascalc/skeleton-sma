68e296a2e1a28c6f5add533bd6f4fd5791d73174
Johan Zeeck <jzeeck@kth.se>
2012-03-22 10:44:40 +0100

    changed count to limit so it corresponds to the new interface to corpus

--- model/tagging_retrieve.rb
+++ model/tagging_retrieve.rb
@@ -6,10 +6,10 @@ class Retrieve
 
 	
 	if(filter.strip.empty?)
-		jsonArray = HTTParty.get("http://nosy.pspace.se:8888/corpus?count=#{limit1}")
+		jsonArray = HTTParty.get("http://nosy.pspace.se:8888/corpus?limit=#{limit1}")
 		puts "@filters was empty"
 	else
-		jsonArray = HTTParty.get("http://nosy.pspace.se:8888/corpus?keywords=#{filter}&count=#{limit1}")
+		jsonArray = HTTParty.get("http://nosy.pspace.se:8888/corpus?keywords=#{filter}&limit=#{limit1}")
 		puts "@filters was #{filter}"
 	end
         return jsonArray   
