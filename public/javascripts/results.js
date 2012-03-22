962f865c5ee3243c0d27c0e5bedfdd942469d330
Niklas Arnell <niklasarnell@hotmail.com>
2012-03-22 19:36:17 +0100

    Added results-files

---
+++ public/javascripts/results.js
@@ -0,0 +1,23 @@
+var lastPostID;
+
+function showFullMessage(postID){
+    var element = $('#' + 'full' + postID);
+    element.removeClass('hidden');
+    element.hide();
+    element.fadeIn('fast');
+
+
+    $('#'+'partial' + postID).addClass('selected');
+
+    var tempID1 = parseInt(lastPostID);
+    var tempID2 = parseInt(postID);
+    if(tempID1!=tempID2){			//Kollar att de två senaste posterna ej samma
+        hideInfo(lastPostID)			//Om ej samma, hideInfo - förra			
+    }
+    lastPostID = postID;
+}
+function hideInfo(postID) {
+    var element=  $('#'+'full' + postID);		//gör till element..
+    element.addClass('hidden'); 			//Ska vara hidden
+    $('#'+'partial' + postID).removeClass('selected');	//info ej selected
+}
