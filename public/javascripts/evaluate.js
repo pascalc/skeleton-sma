2773d4edfeb1bb6bee295028499ef7acab007e60
Christian Magnerfelt <christian.magnerfelt@gmail.com>
2012-03-23 10:45:44 +0100

    updated evaluate js

--- public/javascripts/evaluate.js
+++ public/javascripts/evaluate.js
@@ -21,3 +21,31 @@ function hideInfo(postID) {
     element.addClass('hidden'); 			//Ska vara hidden
     $('#'+'partial' + postID).removeClass('selected');	//info ej selected
 }
+
+$(document).ready(function(){
+
+	// Start stream via AJAX
+	$("#start-button").click(function(e) {
+		e.preventDefault();
+        	$.post("http://localhost:7777/classify/stream", function(data) {
+          		console.log(data);
+        	});
+      	});
+
+	// Juggernaut
+	var show = function(data){
+		line = "<tr>" + data.text + "</tr>";
+		$(line).hide().prependTo("#stream").fadeIn("slow");
+	};
+
+	var jug = new Juggernaut({
+		secure: ('https:' == document.location.protocol),
+		host: document.location.hostname,
+		port: 8080
+	});
+
+	jug.subscribe("nosy", function(data){
+	        show(data);
+        	console.log("Got: " + data);
+      	});
+});
